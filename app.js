01fa7f8d7f4983d4e6c86e69da5cfb6d2a3038bc
EC2 Default User <ec2-user@ip-172-31-33-2.ec2.internal>
2020-12-03 02:30:29 +0000

    section 6

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require("express");
 const app = express();
+const fetch = require("node-fetch");
 
 app.set("view engine", "ejs");
 
 //routes
-app.get("/", function(req, res){
-    res.render("index");
+app.get("/", async function(req, res){
+    
+    let apiUrl = `https://api.unsplash.com/photos/random/?client_id=-3BOO4vv-FqVD_CUht9-NXtA5Pb0NMM0RkPJE6yZjb4&featured=true&orientation=landscape`;
+    let response = await fetch(apiUrl);
+    let data = await response.json();
+    res.render("index", {"imageUrl": data.urls.small});
+});
+
+app.get("/search", async function(req, res){
+    
+    let keyword = "";
+    if (req.query.keyword) {
+        keyword = req.query.keyword;
+    }
+    
+    let apiUrl = `https://api.unsplash.com/photos/random/?count=9&client_id=-3BOO4vv-FqVD_CUht9-NXtA5Pb0NMM0RkPJE6yZjb4&featured=true&orientation=landscape&query=${keyword}`;
+    let response = await fetch(apiUrl);
+    let data = await response.json();
+    
+    let imageUrlArray = [];
+    for (let i = 0; i < data.length; i++){
+        imageUrlArray.push(data[i].urls.small);
+    }
+    res.render("results", {"imageUrl": data[0].urls.small, "imageUrlArray":imageUrlArray});
 });
 
 //starting server
